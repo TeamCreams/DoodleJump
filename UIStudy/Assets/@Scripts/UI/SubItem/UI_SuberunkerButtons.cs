093714b717da85c75f54b07b9c2cd34613b10114
tjdbssy137 <65931605+tjdbssy137@users.noreply.github.com>
2024-08-28 23:49:27 +0900

    [서윤] 숙제

---
+++ UIStudy/Assets/@Scripts/UI/SubItem/UI_SuberunkerButtons.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_SuberunkerButtons : UI_Base
+{
+    public enum Buttons
+    {
+        Retry_Button,
+    }
+
+    protected override void Init()
+    {
+        base.Init();
+        BindButtons(typeof(Buttons));
+
+        this.Get<Button>((int)Buttons.Retry_Button).gameObject.BindEvent((evt) =>
+        {
+            Managers.Scene.LoadScene(Define.EScene.SuberunkerScene);
+        }, Define.EUIEvent.Click);
+    }
+}
